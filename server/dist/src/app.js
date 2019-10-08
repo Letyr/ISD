7873d94935ba6a0ba79172d4326d4a0730927325
Letyr <gofablov@gmail.com>
2019-10-08 16:28:23 +0800

    prepare for deploy

--- server/dist/src/app.js
+++ server/dist/src/app.js
@@ -4,6 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const users_1 = require("./routes/users");
+// import { userController } from './routes/users';
 exports.app = express_1.default();
-exports.app.use('/users', users_1.userController);
+// app.use('/users', userController);
+exports.app.use('/users', (req, res) => {
+    res.send('Hello world!');
+});
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
-import { userController } from './routes/users';
+// import { userController } from './routes/users';
 
 export const app = express();
 
-app.use('/users', userController);
+// app.use('/users', userController);
+
+app.use('/users', (req, res) => {
+    res.send('Hello world!');
+});
