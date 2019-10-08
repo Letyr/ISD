0d3a964c88202a6c7355436f04a21073c53f3847
Letyr <gofablov@gmail.com>
2019-10-08 16:10:22 +0800

    client initial commit

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+
+  constructor(public http: HttpClient) {
+
+  }
+
+  public ngOnInit(): void {
+    this.http.get('http://localhost:3000/users').subscribe({
+      next: response => {
+        console.log(response);
+      }
+    });
+  }
+
+}
