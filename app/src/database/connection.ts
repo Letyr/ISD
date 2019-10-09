5277a31de7ddfb9da28d341260b29e24c921937a
Letyr <gofablov@gmail.com>
2019-10-09 18:18:30 +0800

    ...

--- app/src/database/connection.ts
+++ app/src/database/connection.ts
@@ -1,8 +1,8 @@
 import { Sequelize, Options } from 'sequelize';
 import config from '../../config/config.json';
 
-const dbConfig: Options = (process.env.MODE === 'production' ? config.production : config.development) as Options;
-console.log(process.env.MODE, process.env.MODE === 'production', dbConfig);
+// const dbConfig: Options = (process.env.MODE === 'production' ? config.production : config.development) as Options;
+const dbConfig: Options = (config.production) as Options;
 
 // dbConfig.host = process.env.DATABASE_URL || dbConfig.host;
 
