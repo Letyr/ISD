fe806ff4647cb8a8c949544cda61beeb6fbe8ac3
Letyr <gofablov@gmail.com>
2019-10-09 17:37:03 +0800

    remvoe error handling for throwing it to console

--- app/src/app.ts
+++ app/src/app.ts
@@ -8,22 +8,13 @@ export const app = express();
 
 app.use('/', (req, res) => {
     connection.authenticate()
-    .then(
-        errors => {
-            res.send({
-                msg: `test msg`,
-                connectionEstablish: !Boolean(errors),
-                errors: errors
-            });
-        }
-    )
-    .catch(
-        errors => {
-            res.send({
-                msg: `test msg`,
-                connectionEstablish: !Boolean(errors),
-                errors: errors
-            });
-        }
-    );
+        .then(
+            errors => {
+                res.send({
+                    msg: `test msg`,
+                    connectionEstablish: !Boolean(errors),
+                    errors: errors
+                });
+            }
+        );
 });
