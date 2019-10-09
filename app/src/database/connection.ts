6936f648ce497e62080abf6d382bbdadac40f505
Letyr <gofablov@gmail.com>
2019-10-09 17:57:14 +0800

    create mode variable

--- app/src/database/connection.ts
+++ app/src/database/connection.ts
@@ -2,7 +2,7 @@ import { Sequelize, Options } from 'sequelize';
 import config from '../../config/config.json';
 
 // @TODO зависимость от env.MODE
-const dbConfig: Options = (true ? config.development : config.production) as Options;
+const dbConfig: Options = (process.env.MODE === 'production' ? config.production : config.development) as Options;
 
 dbConfig.host = process.env.DATABASE_URL || dbConfig.host;
 
