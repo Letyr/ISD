56d19aaa85ca0348c062cf7495421079d97612cd
Letyr <gofablov@gmail.com>
2019-10-09 17:34:35 +0800

    add database_url variable

--- app/src/database/connection.ts
+++ app/src/database/connection.ts
@@ -4,4 +4,6 @@ import config from '../../config/config.json';
 // @TODO зависимость от env.MODE
 const dbConfig: Options = (true ? config.development : config.production) as Options;
 
+dbConfig.host = process.env.DATABASE_URL || dbConfig.host;
+
 export const connection = new Sequelize(dbConfig);
