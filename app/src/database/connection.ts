67807c784bbd3c6c06e76d59aaefe9fe8b114477
Letyr <gofablov@gmail.com>
2019-10-09 18:06:24 +0800

    another try...

--- app/src/database/connection.ts
+++ app/src/database/connection.ts
@@ -4,6 +4,6 @@ import config from '../../config/config.json';
 // @TODO зависимость от env.MODE
 const dbConfig: Options = (process.env.MODE === 'production' ? config.production : config.development) as Options;
 
-dbConfig.host = process.env.DATABASE_URL || dbConfig.host;
+// dbConfig.host = process.env.DATABASE_URL || dbConfig.host;
 
 export const connection = new Sequelize(dbConfig);
